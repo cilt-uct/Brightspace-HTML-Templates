4b11c43dbb40410274bfba6408f1e8b113fabaa6
Corné <lightscape2001@gmail.com>
2022-10-04 14:41:19 +0200

    AMA-303: Add Guest Login on Amathuba Landing page

--- public/init.js
+++ public/init.js
@@ -1,3 +1,7 @@
+function insertAfter(newNode, existingNode) {
+    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+}
+
 let header_right = function() {
     const node = document.createElement("div");
     node.setAttribute('class', 'd2l-navigation-header-right');
@@ -32,6 +36,7 @@ let init = function(file) {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     
+    // if guest login in URL
     if (urlParams.get('noRedirect') == 1) {
         // console.log(urlParams.get('noRedirect'));
 
@@ -39,9 +44,45 @@ let init = function(file) {
             const collection = document.getElementsByClassName("d2l-login-portal-flex");
             if (collection.length > 0) {
                 collection[0].classList.add("modal-login");
-                // console.log(collection[0].classList);
             }
         }, 500);
+    }
+
+    // if guest login button
+    const btn = document.getElementById('btn_guest_login');
+    if (btn) {
+        // console.log(btn);
+        btn.addEventListener("click", function(){
+            const collection = document.getElementsByClassName("d2l-login-portal-flex");
+            if (collection.length > 0) {
+                collection[0].classList.add("modal-login");
+            }
+        });
+
+        setTimeout(function () { 
+            const login_btn_container = document.getElementsByClassName("d2l-login-portal-login-container");
+            if (login_btn_container.length > 0) {
+                // console.log(login_btn_container);
+
+                const login_btn = login_btn_container[0].getElementsByClassName('d2l-button');
+                if (login_btn.length > 0) {
+                    // console.log(login_btn);
+
+                    const btn_cancel = document.createElement("button");
+                    btn_cancel.setAttribute('class', 'd2l-button d2l-cancel-button');
+                    btn_cancel.setAttribute('type', 'button');
+                    btn_cancel.textContent = 'Cancel';
+                    btn_cancel.addEventListener("click", function(){
+                        const collection = document.getElementsByClassName("d2l-login-portal-flex");
+                        if (collection.length > 0) {
+                            collection[0].classList.remove("modal-login");
+                        }
+                    });
+
+                    insertAfter(btn_cancel,  login_btn[0]);
+                }
+            }
+        }, 500); // we need to delay - javascript slow loading
     }
 }
 
